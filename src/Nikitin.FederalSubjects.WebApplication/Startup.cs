2e4ca019b155aed492434ff1e7758cdc0fd3d0a0
Victor Maltsev <git@victormaltsev.com>
2022-12-12 02:30:22 +0300

    add project files

---
+++ src/Nikitin.FederalSubjects.WebApplication/Startup.cs
@@ -0,0 +1,70 @@
+﻿using HealthChecks.UI.Client;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using Microsoft.OpenApi.Models;
+
+namespace Nikitin.FederalSubjects.WebApplication;
+
+public class Startup
+{
+    private readonly IConfiguration _configuration;
+
+    public Startup(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.Configure<RouteOptions>(options => { options.LowercaseUrls = true; });
+
+        services.AddControllersWithViews().AddNewtonsoftJson();
+
+        services.AddSwaggerGenNewtonsoftSupport();
+        services.AddSwaggerGen(x =>
+        {
+            x.EnableAnnotations();
+            x.SwaggerDoc("v1", new OpenApiInfo { Title = "Nikitin.FederalSubjects.WebApplication", Version = "v1" });
+        });
+
+        services.AddHealthChecks();
+        services.AddProblemDetails();
+    }
+
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        if (env.IsDevelopment() == true)
+        {
+            app.UseDeveloperExceptionPage();
+
+            app.UseSwagger();
+            app.UseSwaggerUI(x => x.SwaggerEndpoint("/swagger/v1/swagger.json", "Nikitin.FederalSubjects.WebApplication v1"));
+        }
+        else
+        {
+            app.UseExceptionHandler();
+            app.UseHsts();
+        }
+
+        app.UseStatusCodePages();
+
+        app.UseHttpsRedirection();
+        app.UseRouting();
+
+        app.UseAuthorization();
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapControllers();
+
+            endpoints.MapHealthChecks("/health", new HealthCheckOptions
+            {
+                ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+            });
+
+            endpoints.MapHealthChecks("/health/lite", new HealthCheckOptions
+            {
+                ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse,
+                Predicate = (check) => !check.Tags.Contains("deep")
+            });
+        });
+    }
+}
