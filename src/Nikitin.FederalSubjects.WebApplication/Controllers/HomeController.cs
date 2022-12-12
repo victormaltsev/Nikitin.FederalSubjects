1d8e1f41263cb56773217c3a1c71893b92d33912
Victor Maltsev <git@victormaltsev.com>
2022-12-12 04:16:05 +0300

    rename index controller to home controller

--- src/Nikitin.FederalSubjects.WebApplication/Controllers/HomeController.cs
+++ src/Nikitin.FederalSubjects.WebApplication/Controllers/HomeController.cs
@@ -6,12 +6,18 @@ namespace Nikitin.FederalSubjects.WebApplication.Controllers;
 [ApiExplorerSettings(IgnoreApi = true)]
 [Route("[controller]")]
 [Produces(MediaTypeNames.Text.Html)]
-public class IndexController : Controller
+public class HomeController : Controller
 {
     [HttpGet("/")]
     [HttpGet("/index")]
     public IActionResult Index()
     {
         return View();
+    }
+
+    [HttpGet("/not-implemented")]
+    public IActionResult NotImplemented()
+    {
+        throw new NotImplementedException();
     }
 }
