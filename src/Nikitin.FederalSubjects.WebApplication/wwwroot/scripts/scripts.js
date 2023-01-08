8d4173daefc0123e95f0a1078f13f40bafb63e20
Victor Maltsev <git@victormaltsev.com>
2023-01-08 15:34:39 +0300

    tiny refactoring

--- src/Nikitin.FederalSubjects.WebApplication/wwwroot/scripts/scripts.js
+++ src/Nikitin.FederalSubjects.WebApplication/wwwroot/scripts/scripts.js
@@ -1,16 +1,4 @@
 ﻿$(document).ready(function() {
     let link = $(location).attr('pathname').split('/')[1];
-    switch (link) {
-        case 'index':
-            setActive('');
-            break;
-
-        default:
-            setActive(link);
-            break;
-    }
-
-    function setActive(link) {
-        $(`ul.nav a[href="/${link}"]`).removeClass('link-dark').addClass('active');
-    }
+    $(`ul.nav a[href="/${link}"]`).removeClass('link-dark').addClass('active');
 });
